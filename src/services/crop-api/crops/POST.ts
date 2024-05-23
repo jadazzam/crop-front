df995c12fbe8d2240e9bb85fa36a535428eea5fa
jazzam <jad.azzam@avec.fr>
2024-05-23 13:33:40 +0200

    complete param to return trefle object in response

--- src/services/crop-api/crops/POST.ts
+++ src/services/crop-api/crops/POST.ts
@@ -3,7 +3,7 @@ import { withAuth } from "@/services/crop-api/headers";
 import { CROP_API_POST_CROP } from "@/services/crop-api/routes";
 import axios from "axios";
 
-export async function postCrop(data: unknown) {
-  const url = CROP_API_POST_CROP;
+export async function postCrop(data: createType, complete: boolean = true) {
+  const url = `${CROP_API_POST_CROP}?complete=${complete}`;
   return await axios.post(url, data, { headers: await withAuth() });
 }
