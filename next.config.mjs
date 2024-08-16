e313a8c7cb8ec741f93fe946c755fca178559dc0
jazzam <jad.azzam@avec.fr>
2024-08-16 11:31:10 +0200

    clean

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'd2seqvvyy3b8p2.cloudfront.net',
-      //   port: ''
-      //   // pathname: '/trefle/**',
-      // },
       {
         protocol: 'https',
         hostname: 'perenual.com',
